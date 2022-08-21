bbb1f9c0ef3483bb875edd5191ecc17b2bbc62be
Lucas Henrique <lukas.paes18@gmail.com>
2022-08-21 19:03:17 -0300

    feature: clean project

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-500 '>
+      <span className='text-4xl'>event</span>
+    </div>
+  );
+}
