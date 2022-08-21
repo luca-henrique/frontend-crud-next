f9bb007601b5b83e5b4d4147bb2aabf0b37e227c
Lucas Henrique <lukas.paes18@gmail.com>
2022-08-21 19:16:33 -0300

    feature: layout e titulo

---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import Title from './Title';
+
+interface LayoutProps {
+  title: string;
+  children?: any;
+}
+
+export default function Layout({title, children}: LayoutProps) {
+  return (
+    <div className='flex flex-col w-2/3 bg-white text-gray-800 rounded-md'>
+      <Title title={title} />
+
+      <div className='p-6'>{children}</div>
+    </div>
+  );
+}
